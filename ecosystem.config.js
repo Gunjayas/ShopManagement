e01e278733a0af3016bb2614b844fd744ba0997d
gshrestha <gshrestha@digivance.com>
2026-07-27 11:33:06 +0545

    Fixes thrice

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,11 @@ export default {
     {
       name: 'shop-management',
       script: 'server/dist/server/index.js',
+      cwd: '/data/data/com.termux/files/home/ShopManagement',
       env: {
         NODE_ENV: 'production',
+        PORT: '3000',
+        DATABASE_URL: 'file:/data/data/com.termux/files/home/ShopManagement/server/prisma/shop.db',
       },
     },
   ],
